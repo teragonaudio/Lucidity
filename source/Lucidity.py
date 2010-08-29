4f136b7d285b31ce2714a5990a2b3aed56c7bccc
Nik Reiman <nik.reiman@gmail.com>
2010-08-29 13:48:30 +0200

    Table of contents for specification

--- source/Lucidity.py
+++ source/Lucidity.py
@@ -1,1 +1,19 @@
 __author__ = 'nik'
+
+##+
+# @section About Lucidity
+# @section Main Window
+# @subsection Interface Controls
+# @subsection Navigation
+# @subsection Playing Music
+# @subsection Editing Operations
+# @subsection Filtering and Mixing
+# @subsection Applying Effects
+# @section Browsing for Music
+# @section MIDI Mapping
+# @section Key Commands
+# @section Configuring Lucidity
+# @subsection Main Preferences
+# @subsection Sound Card Configuration
+# @subsection MIDI Device Configuration
+# @subsection Plug-In Configuration
