6234e130ebb76247c9c0ed35d7016cd024ada79f
Nik Reiman <nik.reiman@gmail.com>
2010-08-30 22:02:23 +0200

    More key command documentation

--- source/KeyCommands.py
+++ source/KeyCommands.py
@@ -10,18 +10,24 @@ __author__ = 'nik'
 # <tr><td>Esc</td><td>Closes any current pop-up window</td></tr>
 # <tr><td>Ctrl+P</td><td>Start/Stop playback</td></tr>
 # <tr><td>Enter</td><td>Inserts the search selection at the cursor's current position</td></tr>
-# <tr><td>Ctrl+1</td><td>Inserts the search selection at the cursor's timepoint in track 1</td></tr>
-# <tr><td>Ctrl+2</td><td>Inserts the search selection at the cursor's timepoint in track 2</td></tr>
-# <tr><td>Ctrl+3</td><td>Inserts the search selection at the cursor's timepoint in track 3</td></tr>
-# <tr><td>Ctrl+4</td><td>Inserts the search selection at the cursor's timepoint in track 4</td></tr>
-# <tr><td>Ctrl+5</td><td>Inserts the search selection at the cursor's timepoint in track 5</td></tr>
-# <tr><td>Ctrl+6</td><td>Inserts the search selection at the cursor's timepoint in track 6</td></tr>
-# <tr><td>Ctrl+7</td><td>Inserts the search selection at the cursor's timepoint in track 7</td></tr>
-# <tr><td>Ctrl+8</td><td>Inserts the search selection at the cursor's timepoint in track 8</td></tr>
-# <tr><td>Ctrl+9</td><td>Inserts the search selection at the cursor's timepoint in track 9</td></tr>
+# <tr><td>Ctrl+1</td><td>Inserts the search selection at the cursor's position in track 1</td></tr>
+# <tr><td>Ctrl+2</td><td>Inserts the search selection at the cursor's position in track 2</td></tr>
+# <tr><td>Ctrl+3</td><td>Inserts the search selection at the cursor's position in track 3</td></tr>
+# <tr><td>Ctrl+4</td><td>Inserts the search selection at the cursor's position in track 4</td></tr>
+# <tr><td>Ctrl+5</td><td>Inserts the search selection at the cursor's position in track 5</td></tr>
+# <tr><td>Ctrl+6</td><td>Inserts the search selection at the cursor's position in track 6</td></tr>
+# <tr><td>Ctrl+7</td><td>Inserts the search selection at the cursor's position in track 7</td></tr>
+# <tr><td>Ctrl+8</td><td>Inserts the search selection at the cursor's position in track 8</td></tr>
+# <tr><td>Ctrl+9</td><td>Inserts the search selection at the cursor's position in track 9</td></tr>
 # <tr><td>Ctrl+F</td><td>Fullscreen mode</td></tr>
 # <tr><td>Up arrow</td><td>Moves cursor up, automatically deleting any tracks below the cursor without music or mappings</td></tr>
 # <tr><td>Down arrow</td><td>Moves cursor down, automatically adding a new track if at the bottom of the screen</td></tr>
 # <tr><td>Left arrow</td><td>Moves cursor left, automatically zooming in if at the far right of the screen</td></tr>
 # <tr><td>Right arrow</td><td>Moves cursor right, automatically zooming out if at the far right of the screen</td></tr>
 # </table>
+#
+# Note that the "Ctrl" key listed above also refers to the "Control" key on Mac
+# keyboards, not "Command".
+#
+# Lucidity does not allow the user to remap the key commands.  This is done so that
+# the software feels the same on all installations.
