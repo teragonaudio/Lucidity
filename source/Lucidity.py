0970a390aedcaa72e64d723de0d6710edeb02e4a
Nik Reiman <nik.reiman@gmail.com>
2010-08-30 21:20:16 +0200

    About section written
    A bit marketing-speak-ish, but it's gotta be there

--- source/Lucidity.py
+++ source/Lucidity.py
@@ -2,6 +2,24 @@ __author__ = 'nik'
 
 ##+
 # @section About Lucidity
+# Lucidity is software for performing digital music.  It allows the DJ to mix in both
+# realtime and to lay out their mixes a bit in advance.  Lucidity is an uncompromising
+# approach to digital media, and aims to take full advantage of the laptop as a
+# performance tool.
+#
+# The fundamental idea behind Lucidity is that the DJ is planning their mixes out
+# a bit in advance, but not too much.  The amount of preparation time spent in the
+# software itself is minimal, allowing you to focus instead on the music.  The interface
+# resembles a software sequencer, but behaves much differently.  Place the music on the
+# grid, and moves left to the playhead.  While the track is playing, you have time to
+# find your next several tracks and lay them out on the grid.  Effects and filtering can
+# be applied to your transitions in advance, but still manipulated in realtime.
+#
+# Navigation in Lucidity is designed to be simple and quick, and allows for powerful
+# MIDI mapping in comfortably customize the software.  Lucidity's end goal is to be
+# fluid and invisible; an unobtrusive way to play digital media with infinite
+# possibilities.
+#
 # @section Main Window
 # @subsection Interface Controls
 # @subsection Navigation
