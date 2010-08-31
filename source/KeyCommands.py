da10d4f0b0fd19989246e0fe94607f4130cdd529
Nik Reiman <nik.reiman@gmail.com>
2010-08-31 22:17:32 +0200

    Minor grammar corrections

--- source/KeyCommands.py
+++ source/KeyCommands.py
@@ -9,16 +9,16 @@ __author__ = 'nik'
 # <tr><td>Caps Lock</td><td>Turns on/off MIDI mapping mode</td></tr>
 # <tr><td>Esc</td><td>Closes any current pop-up window</td></tr>
 # <tr><td>Ctrl+P</td><td>Start/Stop playback</td></tr>
-# <tr><td>Enter</td><td>Inserts the search selection at the cursor's current position</td></tr>
-# <tr><td>Ctrl+1</td><td>Inserts the search selection at the cursor's position in track 1</td></tr>
-# <tr><td>Ctrl+2</td><td>Inserts the search selection at the cursor's position in track 2</td></tr>
-# <tr><td>Ctrl+3</td><td>Inserts the search selection at the cursor's position in track 3</td></tr>
-# <tr><td>Ctrl+4</td><td>Inserts the search selection at the cursor's position in track 4</td></tr>
-# <tr><td>Ctrl+5</td><td>Inserts the search selection at the cursor's position in track 5</td></tr>
-# <tr><td>Ctrl+6</td><td>Inserts the search selection at the cursor's position in track 6</td></tr>
-# <tr><td>Ctrl+7</td><td>Inserts the search selection at the cursor's position in track 7</td></tr>
-# <tr><td>Ctrl+8</td><td>Inserts the search selection at the cursor's position in track 8</td></tr>
-# <tr><td>Ctrl+9</td><td>Inserts the search selection at the cursor's position in track 9</td></tr>
+# <tr><td>Enter</td><td>Inserts the search selection at the cursor position</td></tr>
+# <tr><td>Ctrl+1</td><td>Inserts the search selection at the cursor position in track 1</td></tr>
+# <tr><td>Ctrl+2</td><td>Inserts the search selection at the cursor position in track 2</td></tr>
+# <tr><td>Ctrl+3</td><td>Inserts the search selection at the cursor position in track 3</td></tr>
+# <tr><td>Ctrl+4</td><td>Inserts the search selection at the cursor position in track 4</td></tr>
+# <tr><td>Ctrl+5</td><td>Inserts the search selection at the cursor position in track 5</td></tr>
+# <tr><td>Ctrl+6</td><td>Inserts the search selection at the cursor position in track 6</td></tr>
+# <tr><td>Ctrl+7</td><td>Inserts the search selection at the cursor position in track 7</td></tr>
+# <tr><td>Ctrl+8</td><td>Inserts the search selection at the cursor position in track 8</td></tr>
+# <tr><td>Ctrl+9</td><td>Inserts the search selection at the cursor position in track 9</td></tr>
 # <tr><td>Ctrl+F</td><td>Fullscreen mode</td></tr>
 # <tr><td>Up arrow</td><td>Moves cursor up, automatically deleting any tracks below the cursor without music or mappings</td></tr>
 # <tr><td>Down arrow</td><td>Moves cursor down, automatically adding a new track if at the bottom of the screen</td></tr>
