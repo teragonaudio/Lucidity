ec0786da6f51c9d56409900c4a43ef4889a3210d
Nik Reiman <nik.reiman@gmail.com>
2010-09-17 00:22:15 +0200

    Adding new classes to setup

--- source/audioDevice/setup.py
+++ source/audioDevice/setup.py
@@ -2,4 +2,5 @@ from distutils.core import setup, Extension
 
 setup(name = "audioDevice", version = "1.0",
       ext_modules = [Extension("audioDevice",
-                               ["ChannelBuffer.c"])])
+                               ["init.c",
+                                "ChannelBuffer.c"])])
