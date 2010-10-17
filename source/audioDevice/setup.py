4912e159e7cdddb5e952867606b145a7b372ef6c
Nik Reiman <nik.reiman@gmail.com>
2010-10-18 01:39:03 +0200

    Basic build file

--- source/audioDevice/setup.py
+++ source/audioDevice/setup.py
@@ -1,9 +1,11 @@
-    from distutils.core import setup, Extension
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
-    setup(name = "audioDevice",
-          version = "1.0",
-          author = "Teragon Audio",
-          ext_modules = [Extension("audiodevice",
-                                   ["AudioDevice.c",
-                                    "ChannelBuffer.c",
-                                    "AudioOutputOSX.c"])])
+ext_modules = [Extension("audiodevice", ["audiodevice.pyx"], extra_link_args=["-framework", "AudioUnit"])]
+
+setup (
+  name = 'audiodevice',
+  cmdclass = {'build_ext': build_ext},
+  ext_modules = ext_modules
+)
