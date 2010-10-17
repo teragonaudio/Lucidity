4a4bf968d44f7cd20fdb2172f5236d20a67248dd
Nik Reiman <nik.reiman@gmail.com>
2010-10-18 01:39:59 +0200

    Adapted test file

--- tests/ChannelBufferTest.py
+++ tests/ChannelBufferTest.py
@@ -1,22 +1,23 @@
-__author__ = 'nik'
-
-import wave
-import audioDevice
-from audioDevice.ChannelBuffer import ChannelBuffer
+#import audiodevice
+#import wave
+from audiodevice import ChannelBuffer, AudioDeviceFactory
 
 if __name__ == "__main__":
     buffer = ChannelBuffer(1024)
-    audioDevice.doStuff(buffer)
+    audioDevice = AudioDeviceFactory().getAudioDevice()
+    audioDevice.initialize()
+    #audioDevice.doStuff(buffer)
     
-    waveFile = wave.open("test.wav", "r")
-    print("File has %d channels, %d frames" % (waveFile.getnchannels(), waveFile.getnframes()))
+    #waveFile = wave.open("test.wav", "r")
+    #print("File has %d channels, %d frames" % (waveFile.getnchannels(), waveFile.getnframes()))
 
 
-    framesRead = 0
-    bufferSize = 512
+    #framesRead = 0
+    #bufferSize = 512
     #while framesRead < waveFile.getnframes():
-    sampleFrames = waveFile.readframes(bufferSize)
+    #sampleFrames = waveFile.readframes(bufferSize)
     #buffer.writeData(sampleFrames)
-    framesRead += bufferSize
+    #framesRead += bufferSize
 
-    waveFile.close()
+    #waveFile.close()
+
