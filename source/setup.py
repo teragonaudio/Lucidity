0645b4875de1bf125a182322d772f5308ff76f02
Nik Reiman <nik.reiman@gmail.com>
2010-11-04 22:01:44 +0100

    Copy SQL files to modules directory

--- source/setup.py
+++ source/setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
 
+mediaExtraFiles = ["*.sql"]
+
 setup(name = "lucidity",
       packages = ['lucidity.media'],
-      package_dir = {'lucidity.media' : 'modules/media'})
+      package_dir = {'lucidity.media' : 'modules/media'},
+      package_data = {'lucidity.media' : mediaExtraFiles})
