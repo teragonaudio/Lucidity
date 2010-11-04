4d8ba1331db5ae01993f98a54fc90bab45ce7c1e
Nik Reiman <nik.reiman@gmail.com>
2010-11-04 21:08:26 +0100

    Adding location, rescanning

--- tests/media/MediaDatabaseTest.py
+++ tests/media/MediaDatabaseTest.py
@@ -1,6 +1,6 @@
 from lucidity.media.MediaDatabase import MediaDatabase
 
 if __name__ == "__main__":
-    print("Rescanning locations")
     mediaDb = MediaDatabase("media.db")
+    mediaDb.addLocation("/Volumes/speedy/Dropbox/Tracks")
     mediaDb.rescan()
