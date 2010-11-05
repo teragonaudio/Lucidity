f9a50990fbc6a4acf883b04048dfeb80532f3d2c
Nik Reiman <nik.reiman@gmail.com>
2010-11-05 19:53:13 +0100

    Defining top-level lucidity module

--- source/setup.py
+++ source/setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 mediaExtraFiles = ["*.sql"]
 
 setup(name = "lucidity",
-      packages = ['lucidity.media'],
-      package_dir = {'lucidity.media' : 'modules/media'},
+      packages = ['lucidity', 'lucidity.media'],
+      package_dir = {'lucidity' : 'modules',
+        'lucidity.media' : 'modules/media'},
       package_data = {'lucidity.media' : mediaExtraFiles})
