22cbf81fcd3c2004c3ec95d121119111db2aa08e
Nik Reiman <nik.reiman@gmail.com>
2010-11-06 18:41:06 +0100

    Removing this field for now

--- source/modules/media/MediaDatabase.py
+++ source/modules/media/MediaDatabase.py
@@ -27,7 +27,7 @@ class MediaFile:
             self.lastModifiedDate = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(lastModifiedTime))
 
     def __dir__(self):
-        return ["id", "absolutePath", "performer", "title", "album"]
+        return ["id", "absolutePath", "title", "album"]
 
     def __str__(self):
         result = None
