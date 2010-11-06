dc93aa71c4219868d22752abdf5e7306c543d471
Nik Reiman <nik.reiman@gmail.com>
2010-11-06 22:07:19 +0100

    Builds audiodevice cython code in setup

--- setup.py
+++ setup.py
@@ -1,19 +1,31 @@
 from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+from platform import platform
+
+link_args = []
+if platform == "Darwin":
+    link_args = [
+        "-framework", "AudioUnit",
+        "-framework", "CoreAudio"
+        ]
 
 setup(name = "lucidity",
+      cmdclass = {'build_ext': build_ext},
       packages = [
           'lucidity',
-          'lucidity.logging',
-          'lucidity.media',
           'id3reader'
           ],
       package_dir = {
-          'lucidity': 'source/modules',
-          'lucidity.logging': 'source/modules/logging',
-          'lucidity.media': 'source/modules/media',
+          'lucidity': 'source/lucidity',
           'id3reader': 'third-party/id3reader'
           },
       package_data = {
-          'lucidity.media': ["*.sql"]
-          }
+          'lucidity': ["*.sql"]
+          },
+      ext_modules = [
+          Extension("lucidity.audiodevice",
+                    ["source/lucidity/audiodevice.pyx"],
+                    extra_link_args = link_args)
+          ],
       )
