eb397c3afffe46e79e88e20d87bbd77e77cdb1dc
Nik Reiman <nik.reiman@gmail.com>
2010-11-06 14:45:14 +0100

    Added global project logger

--- source/modules/logging/Logger.py
+++ source/modules/logging/Logger.py
@@ -1,2 +1,11 @@
-__author__ = 'nik'
-  
+import logging
+
+logger = logging.getLogger("lucidity")
+logger.setLevel(logging.DEBUG)
+
+_consoleHandler = logging.StreamHandler()
+_consoleHandler.setLevel(logging.DEBUG)
+
+_logFormat = logging.Formatter("%(message)s")
+_consoleHandler.setFormatter(_logFormat)
+logger.addHandler(_consoleHandler)
