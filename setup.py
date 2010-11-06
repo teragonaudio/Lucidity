9b7a3efb50c5c83d82a2e7c5e742e1d2d9a4a4c0
Nik Reiman <nik.reiman@gmail.com>
2010-11-06 14:04:08 +0100

    Adding logging package to setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from distutils.core import setup
 setup(name = "lucidity",
       packages = [
           'lucidity',
+          'lucidity.logging',
           'lucidity.media',
           'id3reader'
           ],
       package_dir = {
           'lucidity': 'source/modules',
+          'lucidity.logging': 'source/modules/logging',
           'lucidity.media': 'source/modules/media',
           'id3reader': 'third-party/id3reader'
           },
