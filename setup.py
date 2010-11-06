6db07252a49a82a20876613a9cbd854fff51d89a
Nik Reiman <nik.reiman@gmail.com>
2010-11-06 14:04:00 +0100

    Indenting

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from distutils.core import setup
 
 setup(name = "lucidity",
-      packages = ['lucidity',
-                  'lucidity.media',
-                  'id3reader'],
-      package_dir = {'lucidity' : 'source/modules',
-        'lucidity.media' : 'source/modules/media',
-        'id3reader' : 'third-party/id3reader'},
-      package_data = {'lucidity.media' : ["*.sql"]})
+      packages = [
+          'lucidity',
+          'lucidity.media',
+          'id3reader'
+          ],
+      package_dir = {
+          'lucidity': 'source/modules',
+          'lucidity.media': 'source/modules/media',
+          'id3reader': 'third-party/id3reader'
+          },
+      package_data = {
+          'lucidity.media': ["*.sql"]
+          }
+      )
