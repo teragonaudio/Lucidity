09443d8f704ee33c73b63e52492ca6160d370631
Nik Reiman <nik.reiman@gmail.com>
2010-11-07 13:30:10 +0100

    Fetch platform name correctly

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-from platform import platform
+import sys
 
 link_args = []
-if platform == "Darwin":
     link_args = [
         "-framework", "AudioUnit",
         "-framework", "CoreAudio"
         ]
+if sys.platform == "darwin":
 
 setup(name = "lucidity",
       cmdclass = {'build_ext': build_ext},
