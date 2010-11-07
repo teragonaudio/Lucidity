ed1bba9db20cda6794426e4bcca1b6e20ac73bfd
Nik Reiman <nik.reiman@gmail.com>
2010-11-07 16:37:27 +0100

    Allow searching of other directories if desired

--- tests/mediaTest.py
+++ tests/mediaTest.py
@@ -2,6 +2,10 @@ import sys
 from lucidity.media import MediaDatabase
 
 if __name__ == "__main__":
+    searchArgument = "./tests/resources"
+    if len(sys.argv) == 2:
+        searchArgument = sys.argv[1]
+
     mediaDb = MediaDatabase("media.db")
-    mediaDb.addLocation("./tests/resources")
+    mediaDb.addLocation(searchArgument)
     mediaDb.rescan()
