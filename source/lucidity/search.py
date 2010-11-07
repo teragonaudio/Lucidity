65a20e99c5187b253ce69bb44e77f37670d8ac39
Nik Reiman <nik.reiman@gmail.com>
2010-11-07 17:06:51 +0100

    Basic search function implemented

--- source/lucidity/search.py
+++ source/lucidity/search.py
@@ -1,2 +1,10 @@
-__author__ = 'nik'
-  
+class MediaFinder:
+    def __init__(self, mediaDatabase):
+        self._mediaDatabase = mediaDatabase
+        self._defaultSearchColumns = ['title', 'artist', 'albumArtist', 'album']
+
+    def find(self, searchQuery, inColumns = None):
+        if inColumns is None:
+            inColumns = self._defaultSearchColumns
+        searchResults = self._mediaDatabase.search(searchQuery, inColumns)
+        return searchResults
--- tests/searchTest.py
+++ tests/searchTest.py
@@ -1,2 +1,9 @@
-__author__ = 'nik'
-  
+from lucidity.search import MediaFinder
+from lucidity.media import MediaDatabase
+
+if __name__ == "__main__":
+    mediaDatabase = MediaDatabase("media.db")
+    mediaFinder = MediaFinder(mediaDatabase)
+    searchResults = mediaFinder.find("Album")
+    for mediaFile in searchResults:
+        print(mediaFile)
