d355bfa362a749c33eb1ed9c1816ab52dee15789
Nik Reiman <nik.reiman@gmail.com>
2010-11-07 19:42:09 +0100

    Adding benchmarking for tests

--- tests/mediaTest.py
+++ tests/mediaTest.py
@@ -1,11 +1,15 @@
 import sys
 from lucidity.media import MediaDatabase
+from time import time
 
 if __name__ == "__main__":
     searchArgument = "./tests/resources"
     if len(sys.argv) == 2:
         searchArgument = sys.argv[1]
 
+    startTime = time()
     mediaDb = MediaDatabase("media.db")
     mediaDb.addLocation(searchArgument)
-    mediaDb.rescan()
+    # mediaDb.rescan()
+    stopTime = time()
+    print("Elapsed time: ", stopTime - startTime, "seconds")
--- tests/searchTest.py
+++ tests/searchTest.py
@@ -1,10 +1,17 @@
 from lucidity.search import MediaFinder
 from lucidity.media import MediaDatabase
+from time import time
 
 if __name__ == "__main__":
     mediaDatabase = MediaDatabase("media.db")
+
+    startTime = time()
     mediaFinder = MediaFinder(mediaDatabase)
-    searchResults = mediaFinder.find("syntax")
+    searchResults = mediaFinder.find("and")
+    stopTime = time()
     for mediaFile in searchResults:
         safeString = mediaFile.__str__()
         print(safeString.encode("ascii", "ignore"))
+
+    print("Total files found: ", len(searchResults))
+    print("Elapsed time: ", stopTime - startTime, "seconds")
