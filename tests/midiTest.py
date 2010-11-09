3d1085db7b5fa034987ccdb90d495553ae751793
Nik Reiman <nik.reiman@gmail.com>
2010-11-09 20:19:09 +0100

    Initial midi test file

--- tests/midiTest.py
+++ tests/midiTest.py
@@ -1,2 +1,12 @@
-__author__ = 'nik'
-  
+from lucidity.midi import MidiEventLoop
+from time import sleep
+
+if __name__ == "__main__":
+    midiEventLoop = MidiEventLoop()
+    midiEventLoop.start()
+    midiEventLoop.devices.openInput('UC-33 USB MIDI Controller Port 1')
+    sleep(5)
+    midiEventLoop.devices.rescan()
+    midiEventLoop.terminate()
+    midiEventLoop.join(1)
+    print("MIDI system closed")
