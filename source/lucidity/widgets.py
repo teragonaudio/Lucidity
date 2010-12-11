ce2c5a4cce2f04ad41a2cfb646345461fd4b744c
Nik Reiman <nik.reiman@gmail.com>
2010-12-11 16:51:33 +0100

    Adding widgets module with basic button class

--- source/lucidity/widgets.py
+++ source/lucidity/widgets.py
@@ -1,2 +1,35 @@
-__author__ = 'nik'
-  
+import pygame
+
+class Widget:
+    def __init__(self, rect:"Rect"):
+        self.rect = rect
+
+    def onMouseDown(self): pass
+    def onMouseUp(self): pass
+    def update(self): pass
+
+class Button(Widget):
+    def __init__(self, parentSurface:"Surface", rect:"pygame.Rect",
+                 upImage:"Surface", downImage:"Surface",
+                 onClickHandler:"callable"):
+        Widget.__init__(self, rect)
+        self.upImage = upImage
+        self.downImage = downImage
+        self.activeImage = upImage
+        self.rect.width = self.activeImage.get_width()
+        self.rect.height = self.activeImage.get_height()
+        self._surface = parentSurface
+        self.onClickHandler = onClickHandler
+
+    def onMouseDown(self):
+        self.activeImage = self.downImage
+        self.update()
+
+    def onMouseUp(self):
+        self.activeImage = self.upImage
+        self.update()
+        self.onClickHandler()
+
+    def update(self):
+        self._surface.blit(self.activeImage, self.rect)
+        pygame.display.update(self.rect)
