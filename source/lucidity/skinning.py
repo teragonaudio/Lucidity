977e0e8dc168aad6e54b40945bfaadfd458114e1
Nik Reiman <nik.reiman@gmail.com>
2010-12-11 16:55:22 +0100

    Added skinning class

--- source/lucidity/skinning.py
+++ source/lucidity/skinning.py
@@ -1,2 +1,20 @@
-__author__ = 'nik'
-  
+import pygame
+import os
+
+class Skin:
+    def __init__(self, name:"str"):
+        self.name = name
+        self.images = self._loadImages()
+
+    def _loadImages(self):
+        images = {}
+
+        skinsPath = os.path.abspath("resources/graphics/skins")
+        imagePath = os.path.join(skinsPath, self.name)
+        if not os.path.exists(imagePath):
+            raise Exception("Skins path '" + skinsPath + "' does not exist")
+        for image in os.listdir(imagePath):
+            imageTypeName = os.path.splitext(os.path.basename(image))[0]
+            images[imageTypeName] = pygame.image.load(os.path.join(imagePath, image))
+
+        return images
