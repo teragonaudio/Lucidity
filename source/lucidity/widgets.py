8e7acd3b9943d32acfece2af4e0af3b159427023
Nik Reiman <nik.reiman@gmail.com>
2010-12-12 14:51:17 +0100

    Renaming "update" method to "draw"
    Also passing in parent surface to ctor

--- source/lucidity/widgets.py
+++ source/lucidity/widgets.py
@@ -1,35 +1,36 @@
 import pygame
 
 class Widget:
-    def __init__(self, rect:"Rect"):
+    def __init__(self, parentSurface:"Surface", rect:"Rect"):
+        self.parentSurface = parentSurface
         self.rect = rect
 
     def onMouseDown(self): pass
     def onMouseUp(self): pass
-    def update(self): pass
+    def draw(self): pass
 
 class Button(Widget):
     def __init__(self, parentSurface:"Surface", rect:"pygame.Rect",
                  upImage:"Surface", downImage:"Surface",
                  onClickHandler:"callable"):
-        Widget.__init__(self, rect)
+        Widget.__init__(self, parentSurface, rect)
         self.upImage = upImage
         self.downImage = downImage
         self.activeImage = upImage
         self.rect.width = self.activeImage.get_width()
         self.rect.height = self.activeImage.get_height()
-        self._surface = parentSurface
         self.onClickHandler = onClickHandler
+        self.draw()
 
     def onMouseDown(self):
         self.activeImage = self.downImage
-        self.update()
+        self.draw()
 
     def onMouseUp(self):
         self.activeImage = self.upImage
-        self.update()
         self.onClickHandler()
+        self.draw()
 
-    def update(self):
-        self._surface.blit(self.activeImage, self.rect)
+    def draw(self):
+        self.parentSurface.blit(self.activeImage, self.rect)
         pygame.display.update(self.rect)
