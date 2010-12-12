842c6f5bc5709c57ad7900a3bf769152c1f3921d
Nik Reiman <nik.reiman@gmail.com>
2010-12-12 14:51:52 +0100

    Using sprites for animated objects

--- source/lucidity/panels.py
+++ source/lucidity/panels.py
@@ -1,39 +1,94 @@
 import pygame
-from lucidity.layout import PanelSizer
+from lucidity.layout import Sizing
+from lucidity.sprites import Block
 
 class Panel:
-    def __init__(self, surface, colorChooser, backgroundRect):
-        self._surface = surface
+    def __init__(self, parentSurface:"Surface", rect:"pygame.Rect",
+                 colorChooser:"ColorChooser", skin:"Skin"):
+        self.parentSurface = parentSurface
         self._colorChooser = colorChooser
-        self._backgroundRect = backgroundRect
-        self._backgroundColor = None
+        self.rect = rect
+        self.background = None
+        self.skin = skin
+        self._widgets = []
 
-    def height(self):
-        return self._backgroundRect.height
+    def width(self): return self.rect.width
+    def height(self): return self.rect.height
 
-    def width(self):
-        return self._backgroundRect.width
-
-    def update(self):
-        pygame.display.update(self._backgroundRect)
+    def draw(self): pass
+    def onMouseDown(self, position): pass
+    def onMouseUp(self, position): pass
 
 class MainGrid(Panel):
-    def __init__(self, surface, colorChooser, backgroundRect):
-        Panel.__init__(self, surface, colorChooser, backgroundRect)
-        self._backgroundColor = colorChooser.findColor("Black")
-        pygame.draw.rect(self._surface, self._backgroundColor, self._backgroundRect)
+    def __init__(self, parentSurface:"Surface", rect:"pygame.Rect",
+                 colorChooser:"ColorChooser", skin:"Skin"):
+        Panel.__init__(self, parentSurface, rect, colorChooser, skin)
+        backgroundColor = colorChooser.findColor("Black")
+        self.parentSurface.fill(backgroundColor, self.rect)
+        self.sprites = pygame.sprite.RenderUpdates()
 
-        self._gridRect = pygame.Rect(0, self._backgroundRect.top + PanelSizer.gridPadding,
-                                     self._backgroundRect.width,
-                                     self._backgroundRect.height - (PanelSizer.gridPadding * 2))
-        self._gridColor = self._colorChooser.findColor("Banana Mania") # Heh, yes!
-        pygame.draw.rect(self._surface, self._gridColor, self._gridRect)
+        gridRect = pygame.Rect(0, self.rect.top + Sizing.gridPadding,
+                               self.rect.width,
+                               self.rect.height - (Sizing.gridPadding * 2))
+        self.rect = gridRect
+        self.background = pygame.Surface((gridRect.width, gridRect.height))
+        self.background = self.background.convert()
+        self.background.fill(colorChooser.findColor("Banana Mania"))
+        self.parentSurface.blit(self.background, self.rect)
+        pygame.display.flip()
 
-        self.update()
+    def draw(self):
+        self.sprites.clear(self.parentSurface, self.background)
+        self.sprites.update()
+        updateRects = self.sprites.draw(self.parentSurface)
+        pygame.display.update(updateRects)
+        for sprite in self.sprites.sprites():
+            if not self.rect.colliderect(sprite.rect):
+                sprite.kill()
+
+    def onMouseDown(self, position):
+        pass
+
+    def onMouseUp(self, position):
+        block = Block(pygame.image.load("icon.png"), position)
+        self.sprites.add(block)
+
+    def moveLeft(self):
+        pass
+
+    def moveRight(self):
+        pass
+
+    def moveUp(self):
+        pass
+
+    def moveDown(self):
+        pass
 
 class Toolbar(Panel):
-    def __init__(self, surface, colorChooser, backgroundRect):
-        Panel.__init__(self, surface, colorChooser, backgroundRect)
-        self._backgroundColor = colorChooser.findColor("Gray")
-        pygame.draw.rect(self._surface, self._backgroundColor, self._backgroundRect)
-        self.update()
+    def __init__(self, parentSurface:"Surface", rect:"pygame.Rect",
+                 colorChooser:"ColorChooser", skin:"Skin"):
+        Panel.__init__(self, parentSurface, rect, colorChooser, skin)
+        self.background = pygame.Surface((self.rect.width, self.rect.height))
+        self.background.fill(colorChooser.findColor("Gray"))
+        self.parentSurface.blit(self.background, self.rect)
+        pygame.display.flip()
+
+    def addButton(self, button:"Button"):
+        self._widgets.append(button)
+
+    def draw(self):
+        for widget in self._widgets:
+            if False: #widget.isDirty:
+                self.parentSurface.blit(self.background, widget.rect, widget.rect)
+                widget.draw()
+
+    def onMouseDown(self, position):
+        for widget in self._widgets:
+            if widget.rect.collidepoint(position):
+                widget.onMouseDown()
+
+    def onMouseUp(self, position):
+        for widget in self._widgets:
+            if widget.rect.collidepoint(position):
+                widget.onMouseUp()
