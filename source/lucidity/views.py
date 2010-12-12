f4342863c9e9087d8039e13f44e19bbd0a82a7c9
Nik Reiman <nik.reiman@gmail.com>
2010-12-12 14:51:42 +0100

    Now draws without flickering!

--- source/lucidity/views.py
+++ source/lucidity/views.py
@@ -1,5 +1,6 @@
 import os
 import pygame
+from time import time
 from lucidity.log import logger
 from lucidity.colors import ColorChooser
 from lucidity.keyboard import KeyHandler
@@ -17,74 +18,92 @@ class MainWindow():
         pygame.display.set_caption("Lucidity")
         pygame.display.set_icon(pygame.image.load(os.path.join(".", "icon.png")))
         self._shouldQuit = False
-        self._surface = None
+        self.surface = None
         self._colorChooser = ColorChooser()
         self._keyHandler = KeyHandler()
         self._resolution = (1440, 900)
         self._panelSizer = PanelSizer()
         self._panels = []
         self._skin = Skin("default")
+        self._maxFps = 30
 
     def run(self):
         """
         Open up the window for the application.  This must, sadly, be done in the main
         thread, or else the window will not properly respond to events.
         """
-        windowFlags = pygame.HWSURFACE | pygame.FULLSCREEN
-        self._surface = pygame.display.set_mode((0, 0), windowFlags)
+        windowFlags = pygame.FULLSCREEN
+        self.surface = pygame.display.set_mode(self._resolution, windowFlags)
         self._printVideoInfo(pygame.display.Info())
 
-        self._surface.fill(self._colorChooser.findColor("Black"))
+        self.surface.fill(self._colorChooser.findColor("Black"))
         self._initializeSurfaces(self._resolution, self._skin)
         pygame.display.flip()
 
         logger.info("Initialized display with driver: " + pygame.display.get_driver())
-        while not self._shouldQuit:
-            event = pygame.event.wait()
-            self._processEvent(event)
+        frames = 0
+        initTime = time()
+        frameRenderTimeInSec = 1 / self._maxFps
 
+        while not self._shouldQuit:
+            startTime = time()
+            for event in pygame.event.get():
+                self._processEvent(event)
+
+            for panel in self._panels:
+                panel.draw()
+
+            sleepTime = frameRenderTimeInSec - (time() - startTime)
+            if sleepTime > 0:
+                pygame.time.delay(int(sleepTime * 1000))
+            frames += 1
+
+        totalTime = time() - initTime
+        logger.info("Average FPS: " + str(frames / totalTime))
         pygame.display.quit()
 
     def _initializeSurfaces(self, resolution, skin:"Skin"):
-        mainGrid = MainGrid(self._surface, self._colorChooser,
-                            self._panelSizer.getMainGridRect(resolution[0], resolution[1]))
+        mainGrid = MainGrid(self.surface, self._panelSizer.getMainGridRect(resolution[0], resolution[1]),
+                            self._colorChooser, self._skin)
         self._panels.append(mainGrid)
 
-        topToolbar = Toolbar(self._surface, self._colorChooser,
-                             self._panelSizer.getTopToolbarRect(resolution[0]))
+        topToolbar = Toolbar(self.surface, self._panelSizer.getTopToolbarRect(resolution[0]),
+                             self._colorChooser, self._skin)
         firstButtonRect = pygame.Rect(topToolbar.rect.left + Sizing.toolbarPadding,
                                       topToolbar.rect.top + Sizing.toolbarPadding, 0, 0)
-        leftButton = Button(self._surface, firstButtonRect,
+        leftButton = Button(self.surface, firstButtonRect,
                             skin.images["Left-Arrow-Up"],
                             skin.images["Left-Arrow-Down"],
                             mainGrid.moveLeft)
         topToolbar.addButton(leftButton)
-        rightButton = Button(self._surface, Positioning.rectToRight(leftButton.rect, Sizing.toolbarPadding),
+        rightButton = Button(self.surface, Positioning.rectToRight(leftButton.rect, Sizing.toolbarPadding),
                              skin.images["Right-Arrow-Up"],
                              skin.images["Right-Arrow-Down"],
                              mainGrid.moveRight)
-        upButton = Button(self._surface, Positioning.rectToRight(rightButton.rect, Sizing.toolbarPadding),
+        upButton = Button(self.surface, Positioning.rectToRight(rightButton.rect, Sizing.toolbarPadding),
                           skin.images["Up-Arrow-Up"],
                           skin.images["Up-Arrow-Down"],
                           mainGrid.moveUp)
         topToolbar.addButton(upButton)
-        downButton = Button(self._surface, Positioning.rectToRight(upButton.rect, Sizing.toolbarPadding),
+        downButton = Button(self.surface, Positioning.rectToRight(upButton.rect, Sizing.toolbarPadding),
                             skin.images["Down-Arrow-Up"],
                             skin.images["Down-Arrow-Down"],
                             mainGrid.moveDown)
         topToolbar.addButton(downButton)
         topToolbar.addButton(rightButton)
-
-
         self._panels.append(topToolbar)
 
-        bottomToolbar = Toolbar(self._surface, self._colorChooser,
-                                self._panelSizer.getBottomToolbarRect(resolution[0], resolution[1]))
+        bottomToolbar = Toolbar(self.surface, self._panelSizer.getBottomToolbarRect(resolution[0], resolution[1]),
+                                self._colorChooser, self._skin)
         self._panels.append(bottomToolbar)
 
     def quit(self):
         logger.info("Lucidity is quitting. Bye-bye!")
         self._shouldQuit = True
+
+    def minimize(self):
+        logger.debug("Minimizing")
+        pygame.display.iconify()
 
     def _processEvent(self, event):
         eventType = pygame.event.event_name(event.type)
