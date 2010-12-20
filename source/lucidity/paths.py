b58f4621c5add428f7236b54629bc918ee69137b
Nik Reiman <nik.reiman@gmail.com>
2010-12-20 20:51:56 +0100

    Added ability to get user preference files

--- source/lucidity/paths.py
+++ source/lucidity/paths.py
@@ -1,6 +1,20 @@
 import os
+import platform
 
 class PathFinder:
     @staticmethod
     def findModule(name:"str"):
         return os.path.join(os.path.dirname(__file__), name)
+
+    @staticmethod
+    def findUserFile(name:"str"):
+        platformName = platform.system()
+        if platformName == "Darwin":
+            libraryPath = os.path.expanduser("~/Library/Application Support/Lucidity")
+            if not os.path.exists(libraryPath):
+                os.mkdir(libraryPath)
+            return os.path.abspath(os.path.join(libraryPath, name))
+        else:
+            # If we reach here, then that means we don't know what the OS is, in which
+            # case we just return a path relative to the current executing directory.
+            return os.path.join(".", name)
