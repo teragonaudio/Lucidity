282fca299d7c8799c7766aba02493085770d2434
Nik Reiman <nik.reiman@gmail.com>
2010-12-25 15:06:06 +0100

    Making these variables public

--- source/lucidity/timing.py
+++ source/lucidity/timing.py
@@ -9,9 +9,9 @@ class MusicalClock:
     def __init__(self, tempo:"float"):
         self.tempo = tempo
         self._beatCount = 0
-        self._currentTime = time.time()
-        self._startTime = self._currentTime
-        self._elapsedTime = 0
+        self.currentTime = time.time()
+        self.startTime = self.currentTime
+        self.elapsedTime = 0
         # Note: For the moment, this is hardcoded to be 4/4, but it is possible
         # that this will be changed in the future to allow the user to set the
         # time signature of the arrangement.
@@ -24,9 +24,9 @@ class MusicalClock:
         return self._beatCount / self.timeSignature.beatsPerMeasure
 
     def tick(self):
-        self._currentTime = time.time()
-        self._elapsedTime = self._currentTime - self._startTime
-        self._beatCount = MusicTimeConverter.secondsToBeats(self.tempo, self._elapsedTime)
+        self.currentTime = time.time()
+        self.elapsedTime = self.currentTime - self.startTime
+        self._beatCount = MusicTimeConverter.secondsToBeats(self.tempo, self.elapsedTime)
 
 class MusicTimeConverter:
     @staticmethod
