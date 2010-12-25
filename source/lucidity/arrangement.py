80529b3d577dc4dd3c1267da11f1824ed33318f5
Nik Reiman <nik.reiman@gmail.com>
2010-12-25 15:07:13 +0100

    Items now tied to a track, starting/stop offset

--- source/lucidity/arrangement.py
+++ source/lucidity/arrangement.py
@@ -1,14 +1,19 @@
 from lucidity.timing import MusicalClock
 
 class Item:
-    def __init__(self, label:"str", offset:"int", stackPosition:"int"):
+    def __init__(self, id, track:"int", label:"str", startPositionInBeats:"int",
+                 endPositionInBeats:"int", stackPosition:"int"):
+        self.id = id
+        self.track = track
         self.label = label
-        self.offset = offset
+        self.startPositionInBeats = startPositionInBeats
+        self.endPositionInBeats = endPositionInBeats
         self.stackPosition = stackPosition
 
 class Track:
-    def __init__(self):
+    def __init__(self, id:int):
         self.items = []
+        self.id = id
 
     def addItem(self, item:"Item"):
         self.items.append(item)
@@ -18,11 +23,11 @@ class Sequence:
     MAX_TRACKS = 16
     MIN_TRACKS = 4
 
-    def __init__(self, tempo:"float" = DEFAULT_TEMPO):
+    def __init__(self, tempo:float=DEFAULT_TEMPO):
         self.clock = MusicalClock(tempo)
         self.tracks = []
         for i in range(0, self.MAX_TRACKS):
-            self.tracks.append(Track())
+            self.tracks.append(Track(i))
 
     def getTempo(self):
         return self.clock.tempo
