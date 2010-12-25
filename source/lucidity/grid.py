80b6d2fe80d28013ea21625dba0d978a9b63bd14
Nik Reiman <nik.reiman@gmail.com>
2010-12-25 15:05:52 +0100

    All grid sprite groups refactored to one class

--- source/lucidity/grid.py
+++ source/lucidity/grid.py
@@ -1,85 +1,21 @@
 import pygame
-from lucidity.arrangement import Sequence
+from lucidity.arrangement import Sequence, Item
 from lucidity.layout import Sizing
+from lucidity.log import logger
 from lucidity.containers import Container
 from lucidity.skinning import Skin
-from lucidity.sprites import Block, TrackLine, BarLine
-from lucidity.timing import MusicTimeConverter
-
-class GridSequence:
-    MIN_WIDTH_IN_BARS = 32   # 1 minute @ 120 BPM
-    MAX_WIDTH_IN_BARS = 1024 # 30 minutes @ 120 BPM
-    #DEF_WIDTH_IN_BARS = 120  # 4 minutes @ 120 BPM
-    DEF_WIDTH_IN_BARS = MIN_WIDTH_IN_BARS
-
-    def __init__(self, sequence:"Sequence"):
-        self._sequence = sequence
-        self.widthInBars = self.DEF_WIDTH_IN_BARS
-        self.activeTrackCount = Sequence.MIN_TRACKS
-
-    def expandTracks(self):
-        if self.activeTrackCount < Sequence.MAX_TRACKS:
-            self.activeTrackCount += 1
-
-    def collapseTracks(self):
-        if self.activeTrackCount > Sequence.MIN_TRACKS:
-            self.activeTrackCount -= 1
-
-    def expandBars(self):
-        if self.widthInBars < self.MAX_WIDTH_IN_BARS:
-            self.widthInBars += 4
-
-    def collapseBars(self):
-        if self.widthInBars > self.MIN_WIDTH_IN_BARS:
-            self.widthInBars -= 4
-
-    def getTempo(self):
-        return self._sequence.getTempo()
-
-    def setTempo(self, tempo:"float"):
-        self._sequence.setTempo(tempo)
-
-    def moveLeft(self):
-        self.collapseBars()
-
-    def moveRight(self):
-        self.expandBars()
-
-    def moveUp(self):
-        self.collapseTracks()
-
-    def moveDown(self):
-        self.expandTracks()
-
-class GridSizer:
-    def __init__(self, gridSequence:"GridSequence", rect:"pygame.Rect"):
-        self.gridSequence = gridSequence
-        self.rect = rect
-
-    def getTrackHeightInPx(self):
-        return self.rect.height / self.gridSequence.activeTrackCount
-
-    def getBarWidthInPx(self):
-        return self.rect.width / self.gridSequence.widthInBars
-
-class GridTimer:
-    def __init__(self, gridSequence:"GridSequence"):
-        self.gridSequence = gridSequence
-        self.lastTime = time.time()
-
-    def getWidthInSec(self):
-        return MusicTimeConverter.beatsToSeconds(self.gridSequence.getTempo(), self.gridSequence.widthInBars * 4)
+from lucidity.spritegroups import GridSpriteGroup
 
 class MainGrid(Container):
-    def __init__(self, parentSurface:"Surface", rect:"pygame.Rect", skin:Skin, sequence:"Sequence"):
+    def __init__(self, parentSurface:pygame.Surface, rect:pygame.Rect, skin:Skin, sequence:Sequence):
         Container.__init__(self, parentSurface, rect, skin)
         backgroundColor = skin.guiColor("Background")
         parentSurface.fill(backgroundColor, rect)
+        self.sequence = sequence
 
         gridRect = pygame.Rect(0, rect.top + Sizing.gridPadding,
                                rect.width,
                                rect.height - (Sizing.gridPadding * 2))
-        self.originalTop = gridRect.top
 
         self.parentSurface = parentSurface.subsurface(gridRect)
         self.rect = self.parentSurface.get_rect()
@@ -87,134 +23,33 @@ class MainGrid(Container):
         self.background.fill(skin.guiColor("Grid"))
         self.parentSurface.blit(self.background, self.rect)
 
-        self.gridSequence = GridSequence(sequence)
-        self.gridSizer = GridSizer(self.gridSequence, self.rect)
-        self.gridTimer = GridTimer(self.gridSequence)
-
-        self.gridItems = pygame.sprite.LayeredDirty()
-        self.gridBarLines = pygame.sprite.LayeredDirty()
-        barWidth = self.gridSizer.getBarWidthInPx()
-        for i in range(0, self.gridSequence.widthInBars):
-            self.gridBarLines.add(BarLine(i * 4, (i * barWidth, 0),
-                                          self.rect.height, skin.guiColor("Bar Line"),
-                                          self.getSpeed()))
-
-        self.gridTrackLines = pygame.sprite.LayeredDirty()
-        for i in range(0, Sequence.MAX_TRACKS):
-            self.gridTrackLines.add(TrackLine(i, self.rect.width, skin.guiColor("Track Line")))
-        self.repositionTrackLines()
-
-    def getPixelHeightPerTrack(self):
-        return self.rect.height / self.gridSequence.activeTrackCount
-
-    def repositionTrackLines(self):
-        self.gridTrackLines.clear(self.parentSurface, self.background)
-        pixelsPerTrack = self.getPixelHeightPerTrack()
-        for sprite in self.gridTrackLines.sprites():
-            sprite.visible = int(sprite.index < self.gridSequence.activeTrackCount)
-            if sprite.visible:
-                sprite.setPosition(self.rect.top + pixelsPerTrack * sprite.index)
-            else:
-                sprite.setPosition(-1)
-
-        # TODO: All other sprites must be repositioned accordingly, uck
+        self.gridSprites = GridSpriteGroup(sequence, (gridRect.left, gridRect.top),
+                                           self.rect, skin)
 
     def draw(self):
-        self._drawGridItems()
-        self._drawTrackLines()
-        self._drawBarLines()
-
-    def _drawBarLines(self):
-        self.gridBarLines.clear(self.parentSurface, self.background)
-        self.gridBarLines.update()
-        updateRects = self.gridBarLines.draw(self.parentSurface)
-        for rect in updateRects:
-            rect.top += self.originalTop
-        pygame.display.update(updateRects)
-        numBarLines = len(self.gridBarLines.sprites())
-        if numBarLines < self.gridSequence.widthInBars:
-            lastSprite = self.gridBarLines.sprites()[numBarLines - 1]
-            self.gridBarLines.add(BarLine(lastSprite.valueInBeats + 4,
-                                          (lastSprite.rect.left + self.gridSizer.getBarWidthInPx(), 0),
-                                          self.rect.height, self.skin.guiColor("Bar Line"),
-                                          self.getSpeed()))
-
-    def _drawGridItems(self):
-        self.gridItems.clear(self.parentSurface, self.background)
-
-        now = time.time()
-        elapsedTime = now - self.gridTimer.lastTime
-        self.gridItems.update(elapsedTime)
-        self.gridTimer.lastTime = now
-
-        updateRects = self.gridItems.draw(self.parentSurface)
-        for rect in updateRects:
-            rect.top += self.originalTop
+        self.gridSprites.clear(self.parentSurface, self.background)
+        self.gridSprites.update()
+        updateRects = self.gridSprites.draw(self.parentSurface)
         pygame.display.update(updateRects)
 
-    def _drawTrackLines(self):
-        self.gridTrackLines.clear(self.parentSurface, self.background)
-        self.gridTrackLines.update()
-        updateRects = self.gridTrackLines.draw(self.parentSurface)
-        for rect in updateRects:
-            rect.top += self.originalTop
-        pygame.display.update(updateRects)
-
-    def getSpeed(self):
-        return self.rect.width / self.gridTimer.getWidthInSec()
-
-    def onMouseDown(self, position):
-        pass
+    def onMouseDown(self, position): pass
 
     def onMouseUp(self, position):
-        pixelsPerTrack = self.getPixelHeightPerTrack()
-        nearestTrackLine = None
-        positionY = position[1] - self.originalTop
-
-        for trackLine in self.gridTrackLines.sprites():
-            if trackLine.visible:
-                if positionY > trackLine.rect.top and \
-                   positionY - trackLine.rect.top < pixelsPerTrack:
-                    nearestTrackLine = trackLine
-                    break
-
-        if nearestTrackLine is not None:
-            x = position[0] # TODO: Get nearest bar/beat line
-            y = nearestTrackLine.rect.top + nearestTrackLine.rect.height
-            block = Block((x, y), self.gridSizer.getBarWidthInPx(),
-                          self.gridSizer.getTrackHeightInPx(),
-                          self.skin.nextPaletteColor(),
-                          self.getSpeed())
-            self.gridItems.add(block)
-
-    def recalculateAnimationSpeeds(self, gridBarWidthBefore):
-        if self.gridSequence.widthInBars != gridBarWidthBefore:
-            newSpeed = self.getSpeed()
-            for sprite in self.gridItems.sprites():
-                sprite.speedInPxPerSec = newSpeed
-            for sprite in self.gridBarLines.sprites():
-                sprite.speedInPxPerSec = newSpeed
+        relativePosition = (position[0] - self.absRect.left, position[1] - self.absRect.top)
+        trackNumber = self.gridSprites.getNearestTrackForPosition(relativePosition)
+        barCount = self.gridSprites.getNearestBarForPosition(relativePosition)
+        item = Item(1, trackNumber, "Block", barCount * 4, (barCount + 8) * 4, 0)
+        self.sequence.tracks[trackNumber].addItem(item)
+        self.gridSprites.addItem(item)
 
     def moveLeft(self):
-        gridBarWidthBefore = self.gridSequence.widthInBars
-        self.gridSequence.moveLeft()
-        self.recalculateAnimationSpeeds(gridBarWidthBefore)
+        self.gridSprites.moveLeft()
 
     def moveRight(self):
-        gridBarWidthBefore = self.gridSequence.widthInBars
-        self.gridSequence.moveRight()
-        self.recalculateAnimationSpeeds(gridBarWidthBefore)
-
-    def redrawTrackLines(self, activeTrackCountBefore):
-        if self.gridSequence.activeTrackCount != activeTrackCountBefore:
-            self.repositionTrackLines()
+        self.gridSprites.moveRight()
 
     def moveUp(self):
-        activeTrackCountBefore = self.gridSequence.activeTrackCount
-        self.gridSequence.moveUp()
-        self.redrawTrackLines(activeTrackCountBefore)
+        self.gridSprites.moveUp()
 
     def moveDown(self):
-        activeTrackCountBefore = self.gridSequence.activeTrackCount
-        self.gridSequence.moveDown()
-        self.redrawTrackLines(activeTrackCountBefore)
+        self.gridSprites.moveDown()
---
+++ source/lucidity/spritegroups.py
@@ -0,0 +1,178 @@
+import pygame
+from pygame.sprite import LayeredDirty
+from lucidity.arrangement import Sequence, Item
+from lucidity.skinning import Skin
+from lucidity.sprites import Block, BarLine, TrackLine
+from lucidity.timing import MusicTimeConverter
+
+class GridSpriteGroup(LayeredDirty):
+    MIN_WIDTH_IN_BARS = 32   # 1 minute @ 120 BPM
+    MAX_WIDTH_IN_BARS = 1024 # 30 minutes @ 120 BPM
+    #DEF_WIDTH_IN_BARS = 120  # 4 minutes @ 120 BPM
+    DEF_WIDTH_IN_BARS = MIN_WIDTH_IN_BARS
+
+    def __init__(self, sequence:Sequence, offset:tuple, rect:pygame.Rect, skin:Skin):
+        LayeredDirty.__init__(self)
+        self.sequence = sequence
+        self.offset = offset
+        self.rect = rect
+        self.skin = skin
+        self.blocks = []
+        self.trackLines = []
+        self.barLines = []
+        self.lastTime = sequence.getTime()
+        self.widthInBars = self.DEF_WIDTH_IN_BARS
+        self.activeTrackCount = Sequence.MIN_TRACKS
+
+        self.add(BarLine(0, (0, 0), self.rect.height, skin.guiColor("Bar Line"), self.getSpeed()))
+        for i in range(0, Sequence.MAX_TRACKS):
+            self.add(TrackLine(i, self.rect.width, skin.guiColor("Track Line")))
+        self._updateTrackLines()
+
+    def add(self, *sprites, **kwargs):
+        super().add(*sprites, **kwargs)
+        for sprite in sprites:
+            if isinstance(sprite, Block):
+                self.blocks.append(sprite)
+            elif isinstance(sprite, BarLine):
+                self.barLines.append(sprite)
+            elif isinstance(sprite, TrackLine):
+                self.trackLines.append(sprite)
+
+    def addItem(self, item:Item):
+        firstBar = self.barLines[0]
+        beatsPerBar = self.sequence.clock.timeSignature.beatsPerMeasure
+        positionX = firstBar.rect.left + (((item.startPositionInBeats / beatsPerBar) -
+                                           firstBar.id) * self.getBarWidthInPx())
+        positionY = self.trackLines[item.track].rect.top
+        width = ((item.endPositionInBeats - item.startPositionInBeats) / beatsPerBar) * self.getBarWidthInPx()
+        self.add(Block(item, (positionX, positionY), width, self.getTrackHeightInPx(),
+                       self.skin.nextPaletteColor(), self.skin.font("Block"), self.getSpeed()))
+
+    def remove_internal(self, sprite):
+        super().remove_internal(sprite)
+        if isinstance(sprite, Block):
+            self.blocks.remove(sprite)
+        elif isinstance(sprite, BarLine):
+            self.barLines.remove(sprite)
+        elif isinstance(sprite, TrackLine):
+            self.trackLines.remove(sprite)
+
+    def draw(self, surface, bgd=None):
+        updateRects = super().draw(surface, bgd)
+        # TODO: It's probably more efficient to pass the offset into draw()
+        for rect in updateRects:
+            rect.left += self.offset[0]
+            rect.top += self.offset[1]
+        return updateRects
+
+    def update(self, *args):
+        self._updateBarLines()
+
+        now = self.sequence.getTime()
+        elapsedTime = float(now - self.lastTime)
+        super().update(elapsedTime)
+        self.lastTime = now
+
+    def _updateBarLines(self):
+        lastBarLine = self.barLines[len(self.barLines) - 1]
+        while lastBarLine.rect.left < self.rect.right:
+            nextRect = lastBarLine.rect.move(self.getBarWidthInPx(), 0)
+            barLine = BarLine(lastBarLine.id + 1, (nextRect.left, nextRect.top), self.rect.height,
+                              self.skin.guiColor("Bar Line"), self.getSpeed())
+            self.add(barLine)
+            lastBarLine = barLine
+
+    def _updateGridItemSpeeds(self):
+        speed = self.getSpeed()
+        for sprite in self.barLines:
+            sprite.speedInPxPerSec = speed
+        for sprite in self.blocks:
+            sprite.speedInPxPerSec = speed
+
+    def _updateTrackLines(self):
+        if len(self.trackLines) != self.activeTrackCount:
+            trackHeightInPx = self.getTrackHeightInPx()
+            for trackLine in self.trackLines:
+                trackLine.visible = trackLine.id < self.activeTrackCount
+                if trackLine.visible:
+                    trackLine.setTop(self.rect.top + trackHeightInPx * trackLine.id)
+                else:
+                    trackLine.setTop(-1)
+                trackLine.dirty = 1
+            self._updateBlockHeights(trackHeightInPx)
+
+    def _updateBlockHeights(self, trackHeightInPx:"int"):
+        for block in self.blocks:
+            newRect = block.rect
+            newRect.top = self.trackLines[block.id.track].rect.top
+            newRect.height = trackHeightInPx
+            block.resize(newRect)
+
+    def getSpeed(self):
+        return self.rect.width / self.getWidthInSec()
+
+    def getTrackHeightInPx(self):
+        return self.rect.height / self.activeTrackCount
+
+    def getBarWidthInPx(self):
+        return self.rect.width / self.widthInBars
+
+    def getWidthInSec(self):
+        return MusicTimeConverter.beatsToSeconds(self.getTempo(), self.widthInBars * 4)
+
+    def getTempo(self):
+        return self.sequence.getTempo()
+
+    def setTempo(self, tempo:float):
+        self.sequence.setTempo(tempo)
+
+    def moveLeft(self):
+        self.collapseBars()
+
+    def moveRight(self):
+        self.expandBars()
+
+    def moveUp(self):
+        self.collapseTracks()
+
+    def moveDown(self):
+        self.expandTracks()
+
+    def expandTracks(self):
+        if self.activeTrackCount < Sequence.MAX_TRACKS:
+            self.activeTrackCount += 1
+            self._updateTrackLines()
+
+    def collapseTracks(self):
+        if self.activeTrackCount > Sequence.MIN_TRACKS:
+            self.activeTrackCount -= 1
+            self._updateTrackLines()
+
+    def expandBars(self):
+        if self.widthInBars < self.MAX_WIDTH_IN_BARS:
+            self.widthInBars += 4
+            self._updateGridItemSpeeds()
+
+    def collapseBars(self):
+        if self.widthInBars > self.MIN_WIDTH_IN_BARS:
+            self.widthInBars -= 4
+            self._updateGridItemSpeeds()
+
+    def getNearestTrackForPosition(self, position:"tuple"):
+        trackHeightInPx = self.getTrackHeightInPx()
+        for trackLine in self.trackLines:
+            if trackLine.visible:
+                if position[1] > trackLine.rect.top and \
+                    position[1] - trackLine.rect.top < trackHeightInPx:
+                    return trackLine.id
+        return -1
+
+    def getNearestBarForPosition(self, position:"tuple"):
+        barWidthInPx = self.getBarWidthInPx()
+        for barLine in self.barLines:
+            if barLine.visible:
+                if position[0] > barLine.rect.left and \
+                    position[0] - barLine.rect.left < barWidthInPx:
+                    return barLine.id
+        return -1
