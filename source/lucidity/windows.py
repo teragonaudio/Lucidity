b44885172f1227e991c6a0a99e172676d493ec79
Nik Reiman <nik.reiman@gmail.com>
2010-12-26 15:40:16 +0100

    Moved delegate to separate package

--- source/lucidity/windows.py
+++ source/lucidity/windows.py
@@ -3,8 +3,8 @@ import pygame
 import time
 import lucidity
 from lucidity.arrangement import Sequence
+from lucidity.delegate import MainDelegate
 from lucidity.log import logger
-from lucidity.keyboard import KeyHandler
 from lucidity.grid import MainGrid
 from lucidity.layout import PanelSizer
 from lucidity.midi import MidiEventLoop
@@ -187,79 +187,3 @@ class MainWindow():
 
         for key in videoInfoAttributes.keys():
             logger.debug(videoInfoAttributes[key] + ": " + str(getattr(videoInfo, key)))
-
-# Avoid warnings about unused locals, which is necessary for the event handlers to work
-# properly via reflection
-#noinspection PyUnusedLocal
-class MainDelegate:
-    def __init__(self, mainWindow:"MainWindow"):
-        self.mainWindow = mainWindow
-        self.mainGrid = None
-        self.keyHandler = KeyHandler()
-
-    def processCue(self):
-        pass
-
-    def processMoveLeft(self):
-        self.mainGrid.moveLeft()
-
-    def processMoveRight(self):
-        self.mainGrid.moveRight()
-
-    def processMoveUp(self):
-        self.mainGrid.moveUp()
-
-    def processMoveDown(self):
-        self.mainGrid.moveDown()
-
-    def processActiveEvent(self, eventDict):
-        gain = eventDict['gain']
-        state = eventDict['state']
-        logger.info("Application activated, gain: " + str(gain) + ", state: " + str(state))
-
-    def processKeyDown(self, eventDict):
-        pass
-
-    def processKeyUp(self, eventDict):
-        self.keyHandler.processKey(self, eventDict['key'], eventDict['mod'])
-        pass
-
-    def processMouseButtonDown(self, eventDict):
-        self.mainWindow.processMouseButtonDown(eventDict)
-
-    def processMouseButtonUp(self, eventDict):
-        self.mainWindow.processMouseButtonUp(eventDict)
-
-    def processMouseMotion(self, eventDict):
-        pass
-
-    def processMinimize(self, eventDict = None):
-        self.mainWindow.minimize()
-
-    def processQuit(self, eventDict = None):
-        self.mainWindow.quit()
-
-    def processUndo(self):
-        pass
-
-    def processRedo(self):
-        pass
-
-    def processSelect(self):
-        pass
-
-    def processDelete(self):
-        pass
-
-    def processClone(self):
-        pass
-
-    def processSave(self):
-        pass
-
-    def processSettings(self):
-        pass
-
-    def processReset(self):
-        logger.info("Reset called")
-        self.mainGrid.reset()
