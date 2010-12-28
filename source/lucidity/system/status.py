85cb2330ad995841fc1686264846334e6e34d766
Nik Reiman <nik.reiman@gmail.com>
2010-12-28 14:49:57 +0100

    Only set if status message has changed

--- source/lucidity/system/status.py
+++ source/lucidity/system/status.py
@@ -20,12 +20,16 @@ class StatusLoop(Thread):
 
     def run(self):
         self._isRunning = True
+        lastStatus = None
         while self._isRunning:
             # Sleep first, then update.  This is done because we don't want to
             # write over the initial text right after starting up
             sleep(self.intervalInSec)
             if self.statusProvider is not None and self.delegate is not None:
-                self.delegate.onStatusUpdate(self.statusProvider.getStatusString())
+                status = self.statusProvider.getStatusString()
+                if lastStatus != status:
+                    self.delegate.onStatusUpdate(status)
+                    lastStatus = status
 
     def quit(self):
         self._isRunning = False
