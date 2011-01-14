c55f74b9595067bfd1eb7719f402379fcc8a6c4c
Nik Reiman <nik.reiman@gmail.com>
2011-01-14 16:48:20 +0100

    Added wrapper app project for Mac OSX

---
+++ source/wrapper/mac/main.m
@@ -0,0 +1,29 @@
+//
+//  main.m
+//  Lucidity
+//
+//  Created by Nik Reiman on 14/01/2011.
+//  Copyright 2011 Teragon Audio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[]) {
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  
+  NSBundle *bundle = [NSBundle mainBundle];
+  NSURL *resourcesPath = [[bundle resourceURL] absoluteURL];
+  NSURL *pythonPath = [resourcesPath URLByAppendingPathComponent:@"python/bin/python3.1"];
+  NSURL *lucidityPath = [resourcesPath URLByAppendingPathComponent:@"main.py"];
+  NSArray *arguments = [NSArray arrayWithObject:[lucidityPath path]];
+
+  NSTask *task = [[[NSTask alloc] init] autorelease];
+  [task setLaunchPath:[pythonPath path]];
+  [task setArguments:arguments];
+  [task setCurrentDirectoryPath:[resourcesPath path]];
+  [task launch];
+  [task waitUntilExit];
+  
+  [pool release];
+  return 0;
+}
