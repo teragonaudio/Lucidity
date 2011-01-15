a5b20aaf4c7b228860590ab133593ee836b0aff9
Nik Reiman <nik.reiman@gmail.com>
2011-01-15 17:09:45 +0100

    Build modules correctly

--- source/setup.py
+++ source/setup.py
@@ -1,29 +1,10 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 import sys
 
-platformLinkArgs = []
-
-if sys.platform == "darwin":
-    platformLinkArgs = ['-framework', 'AudioUnit']
-
 setup(name = "lucidity",
-      cmdclass = {'build_ext': build_ext},
-      packages = [
-          'lucidity',
-          'id3reader'
-          ],
-      package_dir = {
-          'lucidity': 'source/lucidity',
-          'id3reader': 'third-party/id3reader'
-          },
-      package_data = {
-          'lucidity': ["*.sql"]
-          },
-      ext_modules = [
-          Extension("lucidity.audiodevice",
-                    ["source/lucidity/audiodevice.py"],
-                    extra_link_args = platformLinkArgs)
-          ],
-      )
+      packages = ['lucidity'],
+      author = "Teragon Audio",
+      package_dir = {'lucidity': 'lucidity'},
+      package_data = {'lucidity': ["*.sql", "*/*.py"]},
+)
